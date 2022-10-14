19cbec7bbf5ceaddd2eb9262469958afeb4a3893
gustavojim <113261314+gustavojim@users.noreply.github.com>
2022-10-14 12:12:45 +0200

    Iteracion 1 2 3

---
+++ vue-basics/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+<Navbar/>
+    <div>
+        {{2 + 2}}
+    </div>
+    <div>      
+        {{holaMundo()}}
+    </div>
+<Footer/>
+
+</template>
+
+<script setup>
+
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+const holaMundo = () => {
+    return 'Hola mundo'
+}
+
+</script>
+
+<style scoped>
+</style>
